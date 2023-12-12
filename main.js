2f73d2b9d442c5378e437bad94b57414da0f5a2b
rustyff12 <russellfrankpitt@googlemail.com>
2023-12-12 05:57:46 +0100

    HTML CSS and JS finished. @media for mobile added

---
+++ main.js
@@ -0,0 +1,46 @@
+
+function playSound(e) {
+        
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+        
+        if(!audio) return   // stop the function from running
+    
+        audio.currentTime = 0;  // rewinds to start of audio to enable rapid sounds
+        audio.play();
+        
+        key.classList.add('playing');
+        // console.log(key);
+    
+}
+
+// creating remove transition function
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if it is not a transform
+    //console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+
+// selecting all elements with the ".key" class
+const keys = document.querySelectorAll('.key');
+// listen for event
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+/*
+// Original that was refactored slightly
+window.addEventListener('keydown', function (e) {
+    // create variables from data-key
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    
+    if(!audio) return   // stop the function from running
+
+    audio.currentTime = 0;  // rewinds to start of audio to enable rapid sounds
+    audio.play();
+    // add class list of playing to give animation
+    key.classList.add('playing');
+    // console.log(key);
+});
+*/
